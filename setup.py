8e0d9637408c9085bf6dbb897c1af18edee5b547
John Miller <jmiller@rjmetrics.com>
2017-02-14 15:40:08 -0500

    update

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
-import os.path
-
+from setuptools import setup
 
 setup(name='tap-braintree',
-      version='0.2.1',
-      description='Taps BrainTree data',
+      version='0.3.0',
+      description='Singer.io tap for extracting data from the Braintree API',
       author='Stitch',
-      url='https://github.com/stitchstreams/tap-braintree',
+      url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_braintree'],
       install_requires=[
-          'stitchstream-python>=0.6.0',
+          'singer-python>=0.1.0',
           'requests==2.12.4',
           'braintree==3.34.0',
       ],
@@ -22,8 +20,9 @@ setup(name='tap-braintree',
       ''',
       packages=['tap_braintree'],
       package_data = {
-          'tap_braintree': [
+          'tap_braintree/schemas': [
               'transactions.json',
           ],
-      }
+      },
+      include_package_data=True,
 )
---
+++ tap_braintree/__init__.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import datetime
+
+import braintree
+import singer
+
+from . import utils
+from .transform import transform_row
+
+
+STATE = {}
+DEFAULT_START_DATE = utils.strftime(datetime.datetime.utcnow() - datetime.timedelta(days=365))
+
+logger = stitchstream.get_logger()
+
+
+def sync_transactions():
+    schema = utils.load_schema("transactions")
+    singer.write_schema("transactions", schema, ["id"])
+
+    now = datetime.datetime.utcnow()
+    start_date = STATE.get('transactions', DEFAULT_START_DATE)
+    while start < now:
+        end = start + datetime.timedelta(days=1)
+        if end > now:
+            end = now
+
+        logger.info("Fetching from {} to {}".format(start, end))
+        data = braintree.Transaction.search(braintree.TransactionSearch.created_at.between(start, end))
+        logger.info("Fetched {} records".format(data.maximum_size, start, end))
+
+        for row in data:
+            transformed = transform_row(row, schema)
+            singer.write_record("transactions", transformed)
+            utils.update_state(STATE, "transactions", transformed["created_at"])
+
+        singer.write_state(STATE)
+        start += datetime.timedelta(days=1)
+
+
+def do_sync():
+    logger.info("Starting sync")
+    sync_transactions()
+    logger.info("Sync completed")
+
+
+def main():
+    args = utils.parse_args()
+    config = utils.load_json(args.config)
+    environment = getattr(braintree.Environment, config.pop("environment", "Production"))
+    braintree.Configuration.configure(environment, **config)
+    if args.state:
+        STATE.update(utils.load_json(args.state))
+    do_sync()
+
+
+if __name__ == '__main__':
+    main()
