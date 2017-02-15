937780a3898c43a53b3bc3f3029ae664ecc5c08c
John Miller <jmiller@rjmetrics.com>
2017-02-15 12:06:26 -0500

    Check for required config keys at startup

--- tap_braintree/__init__.py
+++ tap_braintree/__init__.py
@@ -10,9 +10,15 @@ from .transform import transform_row
 
 
 STATE = {}
-DEFAULT_START_DATE = utils.strftime(datetime.datetime.utcnow() - datetime.timedelta(days=365))
 
 logger = singer.get_logger()
+
+
+def get_start(entity):
+    if entity not in STATE:
+        STATE[entity] = utils.strftime(datetime.datetime.utcnow() - datetime.timedelta(days=365))
+
+    return STATE[entity]
 
 
 def sync_transactions():
@@ -20,7 +26,7 @@ def sync_transactions():
     singer.write_schema("transactions", schema, ["id"])
 
     now = datetime.datetime.utcnow()
-    start = utils.strptime(STATE.get('transactions', DEFAULT_START_DATE))
+    start = utils.strptime(get_state("transactions"))
     while start < now:
         end = start + datetime.timedelta(days=1)
         if end > now:
@@ -35,8 +41,8 @@ def sync_transactions():
             singer.write_record("transactions", transformed)
             utils.update_state(STATE, "transactions", transformed["created_at"])
 
-        singer.write_state(STATE)
         start += datetime.timedelta(days=1)
+        singer.write_state(STATE)
 
 
 def do_sync():
@@ -47,11 +53,15 @@ def do_sync():
 
 def main():
     args = utils.parse_args()
+
     config = utils.load_json(args.config)
+    utils.check_config(config, ["merchant_id", "public_key", "private_key"])
     environment = getattr(braintree.Environment, config.pop("environment", "Production"))
     braintree.Configuration.configure(environment, **config)
+
     if args.state:
         STATE.update(utils.load_json(args.state))
+
     do_sync()
 
 
