caa6473e1afee765252d0bc6f1d56b5471807546
Mike DeLaurentis <miked@stitchdata.com>
2017-04-06 15:05:35 -0400

    Import os

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-braintree',
-      version='0.4.4',
+      version='0.4.5',
       description='Singer.io tap for extracting data from the Braintree API',
       author='Stitch',
       url='http://singer.io',
--- tap_braintree/__init__.py
+++ tap_braintree/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime
+import os
 
 import braintree
 import singer
