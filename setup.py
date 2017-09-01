ce23ebd0c3b9229f41050e18dd7f50192d70c967
Kyle Allan <kyle@stitchdata.com>
2017-09-01 11:26:39 -0400

    bump to 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-braintree',
-      version='0.4.6',
+      version='0.5.0',
       description='Singer.io tap for extracting data from the Braintree API',
       author='Stitch',
       url='http://singer.io',
