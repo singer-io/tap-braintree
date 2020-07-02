245a2f64ee99cc32731c8e7c549b1e7d4befd772
Zack McClelland <zckymc@gmail.com>
2020-07-02 18:05:30 +0100

    Break out status histories into sub streams

--- tap_braintree/__init__.py
+++ tap_braintree/__init__.py
@@ -38,84 +38,41 @@ def to_utc(dt):
     return dt.replace(tzinfo=pytz.UTC)
 
 
-def daterange(start_date, end_date):
-    """
-    Generator function that produces an iterable list of days between the two
-    dates start_date and end_date as a tuple pair of datetimes.
-
-    Note:
-        All times are set to 0:00. Designed to be used in date query where query
-        logic would be record_date >= 2019-01-01 0:00 and record_date < 2019-01-02 0:00
-
-    Args:
-        start_date (datetime): start of period
-        end_date (datetime): end of period
-
-    Yields:
-        tuple: daily period
-            * datetime: day within range
-            * datetime: day within range + 1 day
-
-    """
-
-    # set to start of day
-    start_date = to_utc(
-        datetime.combine(
-            start_date.date(),
-            datetime.min.time(),  # set to the 0:00 on the day of the start date
-        )
-    )
-
-    end_date = to_utc(end_date + timedelta(1))
-
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n), start_date + timedelta(n + 1)
+def write_schemas(schemas: dict):
+    for schema, key_properties in schemas.items():
+        schema_body = load_schema(schema)
+        singer.write_schema(schema, schema_body, key_properties)
 
 
 def sync_merchant_accounts(gateway):
-    schema = load_schema("merchant_accounts")
-
-    singer.write_schema("merchant_accounts", schema, ["id"])
-
+    entity = "merchant_accounts"
     data = gateway.merchant_account.all()
-
     time_extracted = utils.now()
-
     row_written_count = 0
 
     for merchant_account in data.merchant_accounts:
-        transformed = transform_row(merchant_account, schema)
-        singer.write_record(
-            "merchant_accounts", transformed, time_extracted=time_extracted
-        )
+        transformed = transform_row(merchant_account, load_schema(entity))
+        singer.write_record(entity, transformed, time_extracted=time_extracted)
         row_written_count += 1
 
     if row_written_count > 0:
-        logger.info("merchant_accounts: Written {} records".format(row_written_count))
+        logger.info("{}: Written {} records".format(entity, row_written_count))
 
     return data.merchant_accounts
 
 
 def sync_transactions(merchant_accounts, gateway):
-    schema = load_schema("transactions")
-
-    singer.write_schema(
-        "transactions", schema, ["id"], bookmark_properties=["created_at"]
-    )
-
-    latest_updated_at = utils.strptime_to_utc(get_start("transactions"))
-
+    entity = "transactions"
+    latest_updated_at = utils.strptime_to_utc(get_start(entity))
     run_maximum_updated_at = latest_updated_at
-
     period_start = latest_updated_at - TRAILING_DAYS
 
-    logger.info("transactions: Syncing from {}".format(period_start))
-
-    logger.info("transactions: latest_updated_at from {}".format(latest_updated_at))
+    logger.info("{}: Syncing from {}".format(entity, period_start))
+    logger.info("{}: latest_updated_at from {}".format(entity, latest_updated_at))
 
     for merchant in merchant_accounts:
         logger.info(
-            "transactions: Fetching transactions for account {}".format(merchant.id)
+            "{}: Fetching transactions for account {}".format(entity, merchant.id)
         )
 
         time_extracted = utils.now()
@@ -124,8 +81,8 @@ def sync_transactions(merchant_accounts, gateway):
         )
 
         logger.info(
-            "transactions: Fetched {} records for account {}".format(
-                data.maximum_size, merchant.id
+            "{}: Fetched {} records for account {}".format(
+                entity, data.maximum_size, merchant.id
             )
         )
 
@@ -136,8 +93,8 @@ def sync_transactions(merchant_accounts, gateway):
         ]
 
         logger.info(
-            "transactions: {} records to sync for account {}".format(
-                len(rows_to_sync), merchant.id
+            "{}: {} records to sync for account {}".format(
+                entity, len(rows_to_sync), merchant.id
             )
         )
 
@@ -149,7 +106,7 @@ def sync_transactions(merchant_accounts, gateway):
             if not getattr(row, "updated_at"):
                 row.updated_at = row.created_at
 
-            transformed = transform_row(row, schema)
+            transformed = transform_row(row, load_schema(entity))
             updated_at = to_utc(row.updated_at)
 
             # Is this the most recently updated transaction?
@@ -157,64 +114,108 @@ def sync_transactions(merchant_accounts, gateway):
             if updated_at > run_maximum_updated_at:
                 run_maximum_updated_at = updated_at
 
-            singer.write_record(
-                "transactions", transformed, time_extracted=time_extracted
-            )
+            singer.write_record(entity, transformed, time_extracted=time_extracted)
             row_written_count += 1
+
+            logger.info(
+                "{}: Syncing transaction history for transaction {}".format(
+                    entity, row.id
+                )
+            )
+            _sync_transaction_status_history(
+                status_history=row.status_history, transaction_id=row.id
+            )
 
             if row.disputes:
                 logger.info(
-                    "transactions: Fetching disputes for transaction {}".format(row.id)
+                    "{}: Syncing disputes for transaction {}".format(entity, row.id)
                 )
-                _sync_disputes(gateway=gateway, transaction_id=row.id)
+                _sync_disputes(disputes=row.disputes)
 
         if row_written_count:
             logger.info(
-                "transactions: Written {} records for account {}".format(
-                    row_written_count, merchant.id
+                "{}: Written {} records for account {}".format(
+                    entity, row_written_count, merchant.id
                 )
             )
         if row_skipped_count:
             logger.info(
-                "transactions: Skipped {} records for account {}".format(
-                    row_skipped_count, merchant.id
+                "{}: Skipped {} records for account {}".format(
+                    entity, row_skipped_count, merchant.id
                 )
             )
 
     # End day loop
     logger.info(
-        "transactions: Complete. Last updated record: {}".format(run_maximum_updated_at)
+        "{}: Complete. Last updated record: {}".format(entity, run_maximum_updated_at)
     )
 
     latest_updated_at = run_maximum_updated_at
 
-    utils.update_state(STATE, "transactions", latest_updated_at)
+    utils.update_state(STATE, entity, latest_updated_at)
 
     singer.write_state(STATE)
 
 
-def _sync_disputes(gateway, transaction_id):
-    schema = load_schema("disputes")
-    singer.write_schema("disputes", schema, ["id"])
-
-    disputes = gateway.dispute.search(
-        [braintree.DisputeSearch.transaction_id == transaction_id]
-    )
-
+def _sync_disputes(*, disputes):
+    entity = "disputes"
     time_extracted = utils.now()
     row_written_count = 0
-    for dispute in disputes.disputes.items:
-        dispute.transaction_id = transaction_id
-        transformed = transform_row(dispute, schema)
-        singer.write_record("disputes", transformed, time_extracted=time_extracted)
+
+    for dispute in disputes:
+        dispute.transaction_id = dispute.transaction.id
+        transformed = transform_row(dispute, load_schema(entity))
+        singer.write_record(entity, transformed, time_extracted=time_extracted)
+        row_written_count += 1
+        _sync_dispute_status_history(
+            status_history=dispute.status_history, dispute_id=dispute.id
+        )
+
+    if row_written_count > 0:
+        logger.info("{}: Written {} records".format(entity, row_written_count))
+
+
+def _sync_dispute_status_history(*, status_history, dispute_id):
+    entity = "dispute_status_history"
+    time_extracted = utils.now()
+    row_written_count = 0
+
+    for status in status_history:
+        status.dispute_id = dispute_id
+        transformed = transform_row(status, load_schema(entity))
+        singer.write_record(entity, transformed, time_extracted=time_extracted)
         row_written_count += 1
 
     if row_written_count > 0:
-        logger.info("disputes: Written {} records".format(row_written_count))
+        logger.info("{}: Written {} records".format(entity, row_written_count))
+
+
+def _sync_transaction_status_history(*, status_history, transaction_id):
+    entity = "transaction_status_history"
+    time_extracted = utils.now()
+    row_written_count = 0
+
+    for status in status_history:
+        status.transaction_id = transaction_id
+        transformed = transform_row(status, load_schema(entity))
+        singer.write_record(entity, transformed, time_extracted=time_extracted)
+        row_written_count += 1
+
+    if row_written_count > 0:
+        logger.info("{}: Written {} records".format(entity, row_written_count))
 
 
 def do_sync(gateway):
     logger.info("Starting sync")
+    schemas = {
+        "merchant_accounts": ["id"],
+        "transactions": ["id"],
+        "transaction_status_history": ["transaction_id", "status", "timestamp"],
+        "disputes": ["id"],
+        "dispute_status_history": ["dispute_id", "effective_date"],
+    }
+
+    write_schemas(schemas)
     merchants = sync_merchant_accounts(gateway)
     sync_transactions(merchant_accounts=merchants, gateway=gateway)
     logger.info("Sync completed")
