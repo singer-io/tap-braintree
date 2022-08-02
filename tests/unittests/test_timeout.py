ffee9a453c40d17e86dad3dfc13e0fbab1b15de1
Jay Tilala <jay.tilala@CDSYS.LOCAL>
2022-08-02 18:58:31 +0530

    updated unittest

--- tests/unittests/test_timeout.py
+++ tests/unittests/test_timeout.py
@@ -13,6 +13,11 @@ class Mocked():
         self.discover = True
         self.environment = "Sandbox"
 
+
+@mock.patch("tap_braintree.braintree.Environment")
+@mock.patch("tap_braintree.do_discover")
+@mock.patch("tap_braintree.braintree.Configuration")
+@mock.patch("tap_braintree.utils.parse_args")
 class TestTimeout(unittest.TestCase):
     '''
     Test class to validate fetched timeout value
@@ -22,11 +27,7 @@ class TestTimeout(unittest.TestCase):
         ['integer_type_zero_value_in_config', {"request_timeout" : 0, "environment" : "Sandbox"}, None],
         ['string_type_zero_value_in_config', {"request_timeout" : "0", "environment" : "Sandbox"}, None]
     ])
-    @mock.patch("tap_braintree.braintree.Environment")
-    @mock.patch("tap_braintree.do_discover")
-    @mock.patch("tap_braintree.braintree.Configuration")
-    @mock.patch("tap_braintree.utils.parse_args")
-    def test_request_timeout_invalid_value(self, name, test_value, expected_value, mocked_parse_args,mocked_configure, mocked_discover, mocked_environment):
+    def test_request_timeout_invalid_value(self, mocked_parse_args, mocked_configure, mocked_discover, mocked_environment, name, test_value, expected_value):
         '''
         Test function to verify that when invalid value is provide as timeout value
         in config then error is raised
@@ -45,11 +46,7 @@ class TestTimeout(unittest.TestCase):
         ['string_type_timeout_value_in_config', {"request_timeout" : "200", "environment" : "Sandbox"}, 200.0],
         ['integer_type_timeout_value_in_config', {"request_timeout" : 200, "environment" : "Sandbox"}, 200.0]
     ])
-    @mock.patch("tap_braintree.braintree.Environment")
-    @mock.patch("tap_braintree.do_discover")
-    @mock.patch("tap_braintree.braintree.Configuration")
-    @mock.patch("tap_braintree.utils.parse_args" )
-    def test_request_timeout_valid_value(self,name, test_value, expected_value, mocked_parse_args,mocked_configure, mocked_discover, mocked_environment):
+    def test_request_timeout_valid_value(self, mocked_parse_args, mocked_configure, mocked_discover, mocked_environment, name, test_value, expected_value, ):
         """
         Test function to verify when valid timeout value is provided in config and when
         no value is provided in config
