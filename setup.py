5307eb49dd2d415bc37edb7adc605383961ee77a
Ben Allred <ben.allred@qlik.com>
2025-07-21 14:11:49 -0600

    SAC-27925: bump dependencies for compliance (#56)
    * Update setup.py
    
    * Update CHANGELOG.md
    
    * bump version
    
    ---------
    
    Co-authored-by: Bryant Gray <bryant.gray@qlik.com>

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 
 setup(name='tap-braintree',
-      version='0.9.3',
+      version='0.9.4',
       description='Singer.io tap for extracting data from the Braintree API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_braintree'],
       install_requires=[
-          'singer-python==5.5.0',
-          'requests==2.31.0',
+          'singer-python==5.13.2',
+          'requests==2.32.4',
           'braintree==4.18.1',
       ],
       extras_require={
